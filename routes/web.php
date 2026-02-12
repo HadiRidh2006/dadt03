b3087dc4f423ae925b9c0b73f1ba950036536237
Hadi <hadiridhwan2006@gmail.com>
2026-02-12 14:57:05 +0800

    Add HomeController and update home view

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+ 
+// Route::view('/', 'home')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::view('/', 'home')->name('home');
 Route::view('article', 'article')->name('article');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
